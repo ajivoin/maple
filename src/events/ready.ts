d0feec6385f0697260e9e0ee3cf49f56fb6b2aeb
Andrew Jivoin <andrew@jivoin.com>
2026-04-22 14:28:51 -0400

    feat: set Listening to /help activity on bot ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,9 @@
-import { Client, Events } from 'discord.js';
+import { ActivityType, Client, Events } from 'discord.js';
 import { logger } from '../logger.js';
 
 export function registerReady(client: Client): void {
   client.once(Events.ClientReady, (c) => {
     logger.info(`Logged in as ${c.user.tag} (${c.user.id})`);
+    c.user.setActivity('/help', { type: ActivityType.Listening });
   });
 }
